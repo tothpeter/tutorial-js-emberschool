e571c62e6fa96cf8c6d1f97681b0fdd44f6e509b
Peter Toth <tothpeter08@gmail.com>
2018-01-28 14:24:51 +0100

    Create and destroy items, computed properties

--- ember-school-project-1/app/controllers/application.js
+++ ember-school-project-1/app/controllers/application.js
@@ -1,11 +1,22 @@
 import Controller from '@ember/controller';
-import { set } from '@ember/object';
+import { set, computed } from '@ember/object';
 
 export default Controller.extend({
+  menuLength: computed.alias('model.length'),
+  availableItems: computed.filterBy('model', 'isAvailable', true),
   actions: {
     makeUnavailable(food) {
-      set(food, 'isAvailable', false)
+      set(food, 'isAvailable', false);
       food.save();
+    },
+
+    destroyItem(food) {
+      food.destroyRecord();
+    },
+
+    saveNewItem() {
+      this.store.createRecord('food', { name: this.get('newItem') }).save();
+      this.set('newItem', '');
     }
   }
 });
