e87a9ed5ad0a73d0352fb7a7084980003d7add5e
Peter Toth <tothpeter08@gmail.com>
2018-01-29 14:34:59 +0100

    Add edit page of foods

--- ember-school-project-1/app/router.js
+++ ember-school-project-1/app/router.js
@@ -9,6 +9,7 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('foods', function(){
     this.route('food', { path: ':food_id' });
+    this.route('food.edit', { path: ':food_id/edit' });
   });
   this.route('about');
   this.route('contact');
