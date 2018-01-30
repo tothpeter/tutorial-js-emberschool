65bbdefcdc86541b0986ab4921497e9d29a7e4d6
Peter Toth <tothpeter08@gmail.com>
2018-01-30 13:06:20 +0100

    Add collapsable section

--- ember-school-project-1/app/components/food-item.js
+++ ember-school-project-1/app/components/food-item.js
@@ -1,10 +1,16 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  isOpen: false,
+
   actions: {
     toggleAvailability(food) {
       food.toggleProperty('isAvailable');
       food.save();
+    },
+
+    toggleOpen() {
+      this.toggleProperty('isOpen');
     }
   }
 });
