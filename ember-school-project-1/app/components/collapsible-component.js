2c42dbeac95d0a1aba61345108885b549bc4b654
Peter Toth <tothpeter08@gmail.com>
2018-01-30 14:47:05 +0100

    Pass var to block from yield

--- ember-school-project-1/app/components/collapsible-component.js
+++ ember-school-project-1/app/components/collapsible-component.js
@@ -1,10 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  isOpen: false,
+
   actions: {
-    toggleAvailability(food) {
-      food.toggleProperty('isAvailable');
-      food.save();
+    toggleOpen() {
+      this.toggleProperty('isOpen');
     }
   }
 });
