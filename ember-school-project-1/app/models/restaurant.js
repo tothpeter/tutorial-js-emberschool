a1c06f1e411b3f4cc5665057d57c079455620da7
Peter Toth <tothpeter08@gmail.com>
2018-01-31 14:22:14 +0100

    Connect restaurants model with food model

--- ember-school-project-1/app/models/restaurant.js
+++ ember-school-project-1/app/models/restaurant.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   name: attr('string'),
-
+  foods: hasMany()
 });
