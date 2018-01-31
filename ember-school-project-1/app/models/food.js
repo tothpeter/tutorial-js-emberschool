4a2d55f265c845598391d3b8f7ea067096e449ac
Peter Toth <tothpeter08@gmail.com>
2018-01-31 15:14:00 +0100

    Relate restaurant from food model

--- ember-school-project-1/app/models/food.js
+++ ember-school-project-1/app/models/food.js
@@ -1,9 +1,12 @@
 import DS from 'ember-data';
 import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   name: attr('string'),
   imageUrl: attr('string'),
   description: attr('string'),
-  isAvailable: attr('boolean')
+  isAvailable: attr('boolean'),
+
+  restaurant: belongsTo()
 });
