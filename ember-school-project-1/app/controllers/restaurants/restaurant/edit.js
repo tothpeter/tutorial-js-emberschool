75a8d7011b02c90093d2bf650960c5c1ab0278d4
Peter Toth <tothpeter08@gmail.com>
2018-02-01 14:36:16 +0100

    Remove food from restaurant

--- ember-school-project-1/app/controllers/restaurants/restaurant/edit.js
+++ ember-school-project-1/app/controllers/restaurants/restaurant/edit.js
@@ -5,6 +5,16 @@ export default Controller.extend({
     save() {
       this.model.save();
       this.transitionToRoute('restaurants.restaurant', this.model.id);
+    },
+
+    removeFood(food) {
+      let restaurant = this.model;
+
+      restaurant.get('foods').removeObject(food);
+
+      food.save().then(() => {
+        restaurant.save()
+      });
     }
   }
 });
