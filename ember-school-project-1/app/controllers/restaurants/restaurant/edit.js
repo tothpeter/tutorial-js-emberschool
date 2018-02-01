17d74daa42f5a1a01d1bae54ad545b323870a9c0
Peter Toth <tothpeter08@gmail.com>
2018-02-01 14:56:10 +0100

    Implement add food section on edit restaurant page

--- ember-school-project-1/app/controllers/restaurants/restaurant/edit.js
+++ ember-school-project-1/app/controllers/restaurants/restaurant/edit.js
@@ -1,16 +1,28 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  otherFoods: Ember.computed.setDiff('model.foods', 'model.restaurant.foods'),
+
   actions: {
     save() {
-      this.model.save();
-      this.transitionToRoute('restaurants.restaurant', this.model.id);
+      this.model.restaurant.save();
+      this.transitionToRoute('restaurants.restaurant', this.model.restaurant.id);
     },
 
     removeFood(food) {
-      let restaurant = this.model;
+      let restaurant = this.model.restaurant;
 
       restaurant.get('foods').removeObject(food);
+
+      food.save().then(() => {
+        restaurant.save()
+      });
+    },
+
+    addFood(food) {
+      let restaurant = this.model.restaurant;
+
+      restaurant.get('foods').addObject(food);
 
       food.save().then(() => {
         restaurant.save()
--- ember-school-project-1/app/routes/restaurants/restaurant/edit.js
+++ ember-school-project-1/app/routes/restaurants/restaurant/edit.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { hash as RSVPHash } from 'rsvp';
 
 export default Route.extend({
+  model(params) {
+    return RSVPHash({
+      restaurant: this.store.findRecord('restaurant', params.restaurant_id),
+      foods: this.store.findAll('food')
+    });
+  }
 });
