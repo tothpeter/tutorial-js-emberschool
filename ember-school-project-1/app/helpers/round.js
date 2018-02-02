fc9d86d4dd1fc7c29362a9f2f7c021b51b369852
Peter Toth <tothpeter08@gmail.com>
2018-02-02 13:37:40 +0100

    Make the round helper more robust

--- ember-school-project-1/app/helpers/round.js
+++ ember-school-project-1/app/helpers/round.js
@@ -2,7 +2,10 @@ import { helper } from '@ember/component/helper';
 
 export function round(params/*, hash*/) {
   let unroundedNumber = params[0];
-  return Math.round(unroundedNumber * 100) / 100;
+  let roundDecimal = params[1] || 0;
+  let powerOfTen = Math.pow(10, roundDecimal);
+
+  return Math.round(unroundedNumber * powerOfTen) / powerOfTen;
 }
 
 export default helper(round);
