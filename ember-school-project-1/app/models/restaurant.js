5d1680be8b83f6eee0540d0b030d85145238afbb
Peter Toth <tothpeter08@gmail.com>
2018-02-02 13:15:21 +0100

    Practice vision through computed properties

--- ember-school-project-1/app/models/restaurant.js
+++ ember-school-project-1/app/models/restaurant.js
@@ -6,6 +6,15 @@ import { computed } from '@ember/object';
 export default DS.Model.extend({
   name: attr('string'),
   foods: hasMany(),
+
+  avgHealth: computed('foods.@each.health', function() {
+    let totalHealth   = this.get('totalHealth'),
+        numberOfFoods = this.get('foods.length');
+
+    return totalHealth / numberOfFoods;
+  }),
+
+  totalHealth: computed.sum('helathRatings'),
   helathRatings: computed.mapBy('foods', 'health'),
   maxHealth: computed.max('helathRatings'),
   // maxHealth: computed('foods.@each.health', function() {
