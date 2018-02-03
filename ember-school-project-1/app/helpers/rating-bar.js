c822b60cf6d48e893177e6a509d90fa5147ed688
Peter Toth <tothpeter08@gmail.com>
2018-02-03 18:40:10 +0100

    Customize rating bar

--- ember-school-project-1/app/helpers/rating-bar.js
+++ ember-school-project-1/app/helpers/rating-bar.js
@@ -1,12 +1,12 @@
 import { helper } from '@ember/component/helper';
 
-export function ratingBar([rating, inputWidth]/*, hash*/) {
+export function ratingBar([rating, inputWidth], { color = 'blue' }) {
   let percentage = rating * 10;
   let width = inputWidth ? `${inputWidth}px` : '100%'
 
   return Ember.String.htmlSafe(`
     <div class="rating-bar" width="${width}">
-    	<span class="inner-bar" style="width: ${percentage}%"></span>
+    	<span class="inner-bar" style="width: ${percentage}%; background-color: ${color};"></span>
     </div>
   `);
 }
