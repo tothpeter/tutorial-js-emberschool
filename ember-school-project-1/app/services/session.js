2b25202e10b54a5cdcc1934b019145363e31988f
Peter Toth <tothpeter08@gmail.com>
2018-02-05 15:30:24 +0100

    Store logged in user id in cookies

--- ember-school-project-1/app/services/session.js
+++ ember-school-project-1/app/services/session.js
@@ -1,11 +1,29 @@
 import Service from '@ember/service';
 
 export default Service.extend({
+  cookies: Ember.inject.service(),
+  store: Ember.inject.service(),
+
   currentUser: null,
+
+  init() {
+    this._super(...arguments);
+    let currentUserId = this.get('cookies').read('currentUserId');
+
+    if (currentUserId) {
+      this.get('store').findRecord('user', currentUserId).then((user) => {
+        this.set('currentUser', user);
+      })
+
+    }
+  },
+
   login(user) {
     this.set('currentUser', user);
+    this.get('cookies').write('currentUserId', user.id);
   },
   logout() {
     this.set('currentUser', null);
+    this.get('cookies').clear('currentUserId');
   }
 });
